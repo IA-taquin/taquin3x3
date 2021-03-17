b1aad2b0ac9f3df4cc5266b4b50d11c42d1dda8f
b-henriques <75887092+b-henriques@users.noreply.github.com>
2021-03-17 13:49:00 +0100

    reorganisation

--- heuristiques.py
+++ heuristiques.py
@@ -1,4 +1,3 @@
-
 # dictionnaire ième heuristique : [poids élément i]
 poids = {1: [36, 12, 12, 4, 1, 1, 4, 1, 0],
          2: [8, 7, 6, 5, 4, 3, 2, 1, 0],
@@ -23,13 +22,13 @@ def heuristique(etat, poids, coeff, n):
     coeff : coeff de normalisation
     n : nb lignes taquin
     """
-    #somme
+    # somme
     som = 0
     for i, v_i in enumerate(etat):
         # i = pos actuel, v_i pos fianl
         terme = poids[v_i]*dist_elmtr(i, v_i, n)
         som = som + terme
-    #reduction somme <=> div coeff
+    # reduction somme <=> div coeff
     res = som // coeff
     return res
 
@@ -43,14 +42,7 @@ def dist_elmtr(i, v_i, n):
     v_i : la valeur de l'element <=> sa pos final
     n : nb de lignes(=colonnes) du taquin(N*N)
     """
-    #deplacements= abs(pos actuel - pos final)
-    x = abs(i % n - v_i % n) 
+    # deplacements= abs(pos actuel - pos final)
+    x = abs(i % n - v_i % n)
     y = abs(i//n - v_i//n)
     return x+y
-
-
-
-print(heuristique((1, 3, 8, 5, 7, 6, 4, 2, 0),
-                  poids[5], coeff_impair, 3)
-      )
-
