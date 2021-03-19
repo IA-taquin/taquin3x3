68ec785b8ba6c640b311bb69c1acb19fc478f614
b-henriques <75887092+b-henriques@users.noreply.github.com>
2021-03-19 20:58:23 +0100

    v1

--- resolve.py
+++ resolve.py
@@ -1,6 +1,65 @@
+from Noeud import Noeud
 from heuristiques import *
+import heapq  # https://docs.python.org/fr/3/library/heapq.html
+import time
 
-heuristique = lambda etat : fct_heuristique(etat, poids[1], coeff_impair, 3)
 
-#print(heuristique((1, 3, 8, 5, 7, 6, 4, 2, 0)))
+def heuristique(etat): return fct_heuristique(etat, poids[1], coeff_impair, 3)
 
+n = 3
+
+"""
+def recherche(etatinitial)
+	frontier expansion <= {etat initial}
+	ensmble explore <= {etat inital}
+	while true{
+		if FE==empty return false
+		n <= depiler FT
+		if n.etat == etat.final return n
+		fils <= n.expansion()
+		for f in fils{
+			si f a déja été exploré on compare g() ancien et g() nouveau, on remplace si ancien g() > nouveau g()
+			sinon on ajoute f à exploré et FE
+"""
+
+i = 0
+def resolve(etatinitial, posvide):
+    global i
+    # initialisation
+    Noeud.heuristique = heuristique
+    Noeud.n = n
+    frontiere_expansion = []
+    ensemble_explore = {}
+    racine = Noeud(etat = etatinitial, pere = None, mvt="", posvide= posvide)
+    heapq.heappush(frontiere_expansion, racine)
+    while True :
+        i = i+1
+        if (frontiere_expansion==[]): return False
+        noeud = heapq.heappop(frontiere_expansion)
+        if ( noeud.etat == sorted(noeud.etat) ) : return noeud
+        fils = []
+        fils.extend(noeud.expand())
+        for f in fils:
+            deja_explore = ensemble_explore.get(f.adr())
+            if deja_explore == None :
+                # si non explore on ajoute à EE et FE
+                ensemble_explore[f.adr] = f
+                heapq.heappush(frontiere_expansion, f)
+            else:
+                # si deja explore alors
+                #comparaison g() "chemnin"
+                if deja_explore.g() > f.g() : 
+                    deja_explore.pere = f.pere
+                    deja_explore.mvt = f.mvt
+                    heapq.heapify(frontiere_expansion)
+
+deb = time.time()
+n = resolve([1,3,8,5,7,6,4,2,0], 2)
+fin = time.time()
+print(" |solution trouvee en {} sec".format(fin-deb))
+print("%s iterations" %i)
+if(n!=False):
+    print(n.mvts()[::-1])
+    print(n.g())
+else:
+    print("pas de solution trouvee")
--- test_Noeud.py
+++ test_Noeud.py
@@ -6,15 +6,17 @@ class testNoeud(unittest.TestCase):
 
     def test_Noeud_mvt(self):
         Noeud.heuristique = print
-        n1 = Noeud(pere=None, mvt="", etat="init 1", posvide=5)
-        n2 = Noeud(pere=n1, mvt="N", etat="init 2 N", posvide=5)
-        n3 = Noeud(pere=n2, mvt="S", etat="init 3 S", posvide=5)
-        n4 = Noeud(pere=n3, mvt="E", etat="init 4 E", posvide=5)
-        n5 = Noeud(pere=n4, mvt="O", etat="init 5 O", posvide=5)
+        n1 = Noeud(pere=None, mvt="", etat="etat 1", posvide=5)
+        n2 = Noeud(pere=n1, mvt="N", etat="etat 2 atteint en faisant N", posvide=5)
+        n3 = Noeud(pere=n2, mvt="S", etat="etat 3 atteint en faisant S", posvide=5)
+        n4 = Noeud(pere=n3, mvt="E", etat="etat 4 atteint en faisant E", posvide=5)
+        n5 = Noeud(pere=n4, mvt="O", etat="etat 5 atteint en faisant O", posvide=5)
         chemin = n5.mvts()
-        print("chemin = "+chemin)
+        print("chemin de noeud actuel jusqu'à racine = "+chemin)
         self.assertEqual(chemin, "OESN")
-        self.assertEqual(len(chemin), 4)
+        longueur = len(chemin)
+        print("longueur = %s" % longueur)
+        self.assertEqual(longueur, 4)
 
     def test_Noeud_swap(self):
         etat = [0, 1, 2, 3]
@@ -24,3 +26,14 @@ class testNoeud(unittest.TestCase):
         print(etat)
         self.assertEqual(etat, [1, 0, 2, 3])
         
+    def test_Noeud_expand(self):
+        Noeud.heuristique = print
+        Noeud.n = 3
+        n1 = Noeud(etat = [1,3,8,5,7,6,4,2,0], pere = None, mvt="", posvide=2)
+        n1_1 = Noeud(etat = [1,8,3,5,7,6,4,2,0], pere = n1, mvt="O", posvide=1)
+        n1_2 = Noeud(etat = [1,3,6,5,7,8,4,2,0], pere = n1, mvt="S", posvide=5)
+        fils_attendus = [n1_1, n1_2]
+        print("---Expand()---")
+        fils_generes = n1.expand()
+        for f in fils_generes:
+            self.assertIn(f, fils_attendus)
