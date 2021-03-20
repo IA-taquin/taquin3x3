635c6e703bce5b2859c94af029ae7ad31f7271ad
b-henriques <75887092+b-henriques@users.noreply.github.com>
2021-03-20 17:15:18 +0100

    ihm

--- resolve.py
+++ resolve.py
@@ -3,13 +3,19 @@ from heuristiques import *
 import heapq  # https://docs.python.org/fr/3/library/heapq.html
 import time
 
-# changer les heuristiques
-n = 3
-def heuristique(etat): return fct_heuristique(etat, poids[1], coeff_impair, 3)
+
+def choix_heuristique(i, n):
+    """retourne un fct f(etat) qui correspond a l'heuristique h_i
+    """
+    poids_i = poids[i]
+    if i%2==0 : coeff = coeff_pair
+    else: coeff = coeff_impair
+    return lambda etat : fct_heuristique(etat, poids_i, coeff, n)
 
 
-def aSolution(etat, posvide):
-    global n
+def aSolution(etat, posvide, n):
+    """retourne vrai si un taqui a une solution, faux sinon
+    """
     l = copy(etat)
     e_posvide = dist_elmtr(posvide, etat[posvide], n)
     cpt = 0
@@ -25,9 +31,12 @@ def aSolution(etat, posvide):
 
 
 
-i = 0
-def resolve(etatinitial, posvide):
-    global i
+def resolve(etatinitial, posvide, heuristique, n):
+    """renvoi un noeud qui correspond a l'etat de but et qui peut donc etre utiliser pour reconstruire la solution
+    renvoi (noeud, i, r) où i=nb iterations, r=nb remplacements dans la file priorite    
+    """
+    i = 0 #nb iterations boucle while
+    r = 0 #nb de remplacements
     # initialisation
     Noeud.heuristique = heuristique
     Noeud.n = n
@@ -39,7 +48,7 @@ def resolve(etatinitial, posvide):
         i = i+1
         if (frontiere_expansion==[]): return False
         noeud = heapq.heappop(frontiere_expansion)
-        if ( noeud.etat == [0,1,2,3,4,5,6,7,8] ) : return noeud
+        if ( noeud.etat == [0,1,2,3,4,5,6,7,8] ) : return noeud, i, r
         fils = []
         fils.extend(noeud.expand())
         for f in fils:
@@ -51,19 +60,9 @@ def resolve(etatinitial, posvide):
             else:
                 # si deja explore alors
                 #comparaison g() "chemnin"
-                if deja_explore.g() > f.g() : 
+                if  f.g() < deja_explore.g() :
+                    r += 1 
                     deja_explore.pere = f.pere
                     deja_explore.mvt = f.mvt
                     heapq.heapify(frontiere_expansion)
 
-
-deb = time.time()
-noeud = resolve([1,3,8,5,7,6,4,2,0], 2)
-fin = time.time()
-print(" |solution trouvee en {} sec".format(fin-deb))
-print("%s iterations" %i)
-if(noeud!=False):
-    print(noeud.mvts()[::-1])
-    print(noeud.g())
-else:
-    print("pas de solution trouvee")
