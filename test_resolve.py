115391099e37981f157f8e0bbda60d36d08987dc
b-henriques <75887092+b-henriques@users.noreply.github.com>
2021-03-20 21:14:53 +0100

    v_final

--- test_resolve.py
+++ test_resolve.py
@@ -4,5 +4,5 @@ from resolve import aSolution
 class testResolve(unittest.TestCase):
 
     def test_aSolution(self):
-        self.assertTrue(aSolution([1,3,8,5,7,6,4,2,0], 2))
-        self.assertFalse(aSolution([8,6,7,2,5,4,3,0,1], 0))
+        self.assertTrue(aSolution([1,3,8,5,7,6,4,2,0], 2, 3))
+        self.assertFalse(aSolution([8,6,7,2,5,4,3,0,1], 0, 3))
